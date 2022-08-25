95019d13bb69aca6e618395d0b8b595406d20fef
muhammad ruhiyat <muhammadruhiyat020998@gmail.com>
2022-08-25 14:09:20 +0700

    merubah interface menambahkan category

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -8,7 +8,7 @@
     <h1>
     <a href="/posts/{{ $pos->slug }}" class="text-decoration-none">{{ $pos->tittle}}</a>
     </h1>
-    <h5>by: {{ $pos->author }}</h5>
+    <h5>by: {{ $pos->author }} in {{ $pos->category->name }}</h5>
     <p>{{ $pos->excerpt }}</p>
 
 </article>
