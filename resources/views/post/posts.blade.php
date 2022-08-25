884fbe204da3a4662452165d551e06c7097de156
muhammad ruhiyat <muhammadruhiyat020998@gmail.com>
2022-08-25 14:10:04 +0700

    merubah interface

--- resources/views/post/posts.blade.php
+++ resources/views/post/posts.blade.php
@@ -5,7 +5,7 @@
 <article class="mb-5">
 
 <h1> {{ $post->tittle }} </h1>
-<h5>{{ $post->author }}</h5>
+<h5>By : {{ $post->author }} in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a> </h5>
 {!! $post->body !!}
 
 </article>
