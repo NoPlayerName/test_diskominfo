63865f5f1514099b8b87f78cbbbd3c0b6b069aba
muhammad ruhiyat <muhammadruhiyat020998@gmail.com>
2022-08-25 13:58:53 +0700

    merubah tampilan index category

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -1,4 +1,16 @@
 @extends('layouts.main')
 @section('container')
-{{ $tittle }}    
+<h1 class="mb-5">{{ $tittle }}</h1>
+
+@foreach ($category as $cat)
+<article class="mb-5">
+
+    <h1>
+    <a href="/categories/{{ $cat->slug }}" class="text-decoration-none">{{ $cat->name}}</a>
+    </h1>
+ 
+
+</article>
+@endforeach
+    
 @endsection
