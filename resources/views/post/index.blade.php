9e8131e8b8846002ef22dd03bfe615761c0f6b5a
muhammad ruhiyat <muhammadruhiyat020998@gmail.com>
2022-08-25 14:59:54 +0700

    menambahkan read more dan menambahkan border

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -3,15 +3,16 @@
 {{ $tittle }}
 
 @foreach ($post as $pos)
-<article class="mb-5">
+<article class="mb-5 border-bottom pb-4">
 
     <h1>
     <a href="/posts/{{ $pos->slug }}" class="text-decoration-none">{{ $pos->tittle}}</a>
     </h1>
     <h5>by: {{ $pos->author }} in {{ $pos->category->name }}</h5>
     <p>{{ $pos->excerpt }}</p>
-
+    <a href="/posts/{{ $pos->slug }}" class="text-decoration-none">Read More</a>
 </article>
+
 @endforeach
     
 @endsection
