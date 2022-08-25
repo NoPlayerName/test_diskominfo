f24e114643e27860578985f83c1fde44763d31e9
muhammad ruhiyat <muhammadruhiyat020998@gmail.com>
2022-08-25 15:00:51 +0700

    menambahkan seed untuk mengisi data base

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,10 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Post;
+use App\Models\User;
+use App\Models\Category;
+use PhpParser\Node\Expr\AssignOp\Pow;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +24,35 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        User::create([
+            'name' => 'Muhammad Ruhiyat',
+            'email' => 'muhammadruhiyat@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming'
+        ]);
+
+        Category::create([
+            'name' => 'Web Design',
+            'slug' => 'web-design'
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::create([
+            'tittle' => 'Judul Permata',
+            'slug' => 'judul-pertama',
+            'excerpt' => ' ',
+            'body' => ' ',
+            'category_id' => '1',
+            'user_id' => '1'
+        ]);
     }
 }
