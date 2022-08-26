81d63cf1e0de65030d3343fb1e4191672b693d37
muhammad ruhiyat <muhammadruhiyat020998@gmail.com>
2022-08-26 11:08:31 +0700

    menambahkan tag a untuk link author

--- resources/views/post/posts.blade.php
+++ resources/views/post/posts.blade.php
@@ -5,7 +5,8 @@
 <article class="mb-5">
 
 <h1> {{ $post->tittle }} </h1>
-<h5>By : <a href="" class="text-decoration-none"> {{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a> </h5>
+<h5>By : <a href="" class="text-decoration-none"> {{ $post->user->name }}</a> in
+ <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a> </h5>
 {!! $post->body !!}
 
 </article>
