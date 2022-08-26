1d2ccfcf5ad7f236d5041176f7a02dc91177fd60
muhammad ruhiyat <muhammadruhiyat020998@gmail.com>
2022-08-26 15:20:12 +0700

    menambahkan link direct pada tag a

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -8,7 +8,7 @@
     <h1>
     <a href="/posts/{{ $pos->slug }}" class="text-decoration-none">{{ $pos->tittle}}</a>
     </h1>
-    <h5>by: <a href="" class="text-decoration-none"> {{ $pos->user->name }}</a> in <a href="/categories/{{ $pos->category->slug }}" class="text-decoration-none">{{ $pos->category->name }}</a></h5>
+    <h5>by: <a href="/author/{{ $pos->user->id }}" class="text-decoration-none"> {{ $pos->user->name }}</a> in <a href="/categories/{{ $pos->category->slug }}" class="text-decoration-none">{{ $pos->category->name }}</a></h5>
     <p>{{ $pos->excerpt }}</p>
     <a href="/posts/{{ $pos->slug }}" class="text-decoration-none">Read More</a>
 </article>
