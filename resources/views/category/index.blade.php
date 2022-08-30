a47e669fb4cf2b87941943e7079a9514386eff5f
muhammad ruhiyat <muhammadruhiyat020998@gmail.com>
2022-08-30 15:20:03 +0700

    merumah design posts

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -2,15 +2,25 @@
 @section('container')
 <h1 class="mb-5">{{ $tittle }}</h1>
 
-@foreach ($category as $cat)
-<article class="mb-5">
 
-    <h1>
-    <a href="/categories/{{ $cat->slug }}" class="text-decoration-none">{{ $cat->name}}</a>
-    </h1>
- 
 
-</article>
-@endforeach
+
+<div class="container">
+    <div class="row">
+        @foreach ($category as $cat)
+        <div class="col-md-4">
+            <a href="/categories/{{ $cat->slug }}" class="text-decoration-none">
+            <div class="card text-bg-dark">
+                <img src="https://source.unsplash.com/500x400?{{ $cat->name }}" class="card-img" alt="...">
+                <div class="card-img-overlay d-flex align-items-center p-0">
+                  <h5 class="card-title text-center flex-fill p-4 fs-3" style="background-color: rgba(0,0,0,0.7); ">{{ $cat->name}}</h5>
+                </div>
+              </div>
+            </a>
+        </div>
+        @endforeach
+    </div>
+</div>
     
 @endsection
+
