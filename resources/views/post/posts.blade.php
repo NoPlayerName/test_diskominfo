78843f2eae4d80ddb50fa1c06dec824c8100933f
muhammad ruhiyat <muhammadruhiyat020998@gmail.com>
2022-08-30 10:19:40 +0700

    redesign UI

--- resources/views/post/posts.blade.php
+++ resources/views/post/posts.blade.php
@@ -2,16 +2,21 @@
 @section('container')
 {{-- {{ $tittle }} --}}
 
-<article class="mb-5">
+<div class="container">
+<div class="row justify-content-center">
+    <div class="col-md-8">
+        <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" class="card-img-top rounded-top" alt="...">
+        <h1> {{ $post->tittle }} </h1>
+        <h5>By : <a href="/author/{{ $post->author->username }}" class="text-decoration-none"> {{ $post->author->name }}</a> in
+         <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a> </h5>
+        {!! $post->body !!}
+        <a href="/post" class="d-block mt-3"> Back to Post</a>
+    </div>
+</div>
 
-<h1> {{ $post->tittle }} </h1>
-<h5>By : <a href="/author/{{ $post->author->username }}" class="text-decoration-none"> {{ $post->author->name }}</a> in
- <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a> </h5>
-{!! $post->body !!}
+</div>
 
-</article>
 
-<a href="/post"> Back to Post</a>
 
     
 @endsection
