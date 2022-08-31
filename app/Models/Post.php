c667c7c3ec8d90f393d891ca3b69d45a14bbe8e2
muhammad ruhiyat <muhammadruhiyat020998@gmail.com>
2022-08-31 09:43:32 +0700

    menambahkan function scope

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,6 +5,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use PhpParser\Node\Expr\Isset_;
 
 class Post extends Model
 {
@@ -22,5 +23,21 @@ class Post extends Model
     public function author()
     {
         return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function scopeSearch($query, array $fillter)
+    {
+        $query->when($fillter['search'] ?? false, function ($query, $search) {
+            return $query->where(function ($query) use ($search) {
+                $query->where('tittle', 'like', '%' . $search . '%')
+                    ->orWhere('body', 'like', '%' . $search . '%');
+            });
+        });
+
+        $query->when($fillter['category'] ?? false, function ($query, $category) {
+            return $query->whereHas('category', function ($query) use ($category) {
+                $query->where('slug', $category);
+            });
+        });
     }
 }
