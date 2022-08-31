010af4399a8d8992fd19e04e57d113cd0e2037b9
muhammad ruhiyat <muhammadruhiyat020998@gmail.com>
2022-08-31 11:30:07 +0700

    menambahkan request author dan paginate withquerystring

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,18 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Models\User;
 
 class PostController extends Controller
 {
     public function index()
     {
 
+        $tittle = '';
+        if (request('category')) {
+            $category = Category::firstWhere('slug', request('category'));
+            $tittle = " in " . $category->name;
+        }
+        if (request('author')) {
+            $author =  User::firstWhere('username', request('author'));
+            $tittle = " by " . $author->name;
+        }
+
         // dd(request('search'));
         return view('post.index', [
-            'tittle' => "Halaman Post",
-            'post' => Post::latest()->search(request(['search', 'category']))->get()
+            'tittle' => "Halaman Post" . $tittle,
+            'post' => Post::latest()->search(request(['search', 'category', 'author']))->paginate(7)->withQueryString()
         ]);
     }
 
