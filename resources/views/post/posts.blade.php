12c1418b6a1915ae04d8949d2f9f07a10602ea3b
muhammad ruhiyat <muhammadruhiyat020998@gmail.com>
2022-08-31 11:35:28 +0700

    mengganti direct category dan author

--- resources/views/post/posts.blade.php
+++ resources/views/post/posts.blade.php
@@ -6,8 +6,8 @@
 <div class="row justify-content-center mb-5">
     <div class="col-md-8">
         <h1 class="mb-3"> {{ $post->tittle }} </h1>
-        <p>By : <a href="/author/{{ $post->author->username }}" class="text-decoration-none"> {{ $post->author->name }}</a> in
-         <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a> </p>
+        <p>By : <a href="/post?author={{ $post->author->username }}" class="text-decoration-none"> {{ $post->author->name }}</a> in
+         <a href="/post?category={{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a> </p>
         <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" class="card-img-top rounded-top" alt="...">
         {!! $post->body !!}
         <a href="/post" class="d-block mt-3"> Back to Post</a>
