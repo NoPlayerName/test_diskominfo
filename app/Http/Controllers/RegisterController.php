2318c47d52702289f8da7b33e268a9553790cbbe
muhammad ruhiyat <muhammadruhiyat020998@gmail.com>
2022-09-01 15:59:20 +0700

    menambahkan vlaidasi untuk register

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+// untuk menambahkan validasi password kombinasi
+use Illuminate\Validation\Rules\Password; 
+use Mockery\Generator\StringManipulation\Pass\Pass;
 
 class RegisterController extends Controller
 {
@@ -11,5 +16,26 @@ class RegisterController extends Controller
         return view('auth.register',[
             'tittle' => 'Register'
         ]);
+    }
+
+    public function store(Request $request)
+    {
+        // return $request->all();
+        $dataValidate = $request->validate([
+            'name' => 'required|max:255',
+            'username' => 'required|min:3|max:255|unique:users',
+            'email' => ['required', 'email:dns', 'unique:users'],
+            'password' => ['required', 'min:3', 'max:25']
+            // 'password' => ['required', Password::min(8)->mixedCase()->numbers()] validasi tambahan untuk password kombinasi
+
+            // 'name' => ['required', 'max:255'],
+            // 'username' => ['required', 'min:3', 'max:255', 'unique:users'],
+            // 'email' => ['required', 'email:dns', 'unique:users'],
+        ]);
+
+        $dataValidate['password'] = bcrypt($dataValidate['password']);
+        User::create($dataValidate);
+
+
     }
 }
